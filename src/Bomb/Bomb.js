96f2057fdbbbb9285aad308ad9fc64a2353395d5
Gage <gagemigan@gmail.com>
2020-06-17 12:50:25 -0400

    hello is done

---
+++ src/Bomb/Bomb.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Bomb extends React.Component{
+    constructor(props){
+        this.state ={
+            counter : 0 ,
+        }
+    }
+    render(){
+        return(
+            <div className="Bomb-button">
+
+
+            </div>
+        )
+    }
+
+}
+
+
+export default Bomb
